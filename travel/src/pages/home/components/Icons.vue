ef172362e021eec8a144829c23c1521eea279b02
littlebirdflying <tongmengwang@163.com>
2018-11-14 07:19:19 +0800

    vue-icon区布局

---
+++ travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="icons">
+  <div class="icon">
+    <div class="icon-img">
+      <img class="icon-img-content" src="https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+    </div>
+    <p class="icon-desc">景点门票</p>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+<style lang="stylus" scoped>
+.icons
+  overflow hidden
+  height 0
+  padding-bottom 50%
+  background green
+  .icon
+    position relative
+    overflow hidden
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    background red
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      background blue
+      .icon-img-content
+        height 100%
+</style>
