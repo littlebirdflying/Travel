78c1be5efe689cf2ca481659d6bae03dd326125d
littlebirdflying <tongmengwang@163.com>
2018-12-19 07:07:37 +0800

    性能优化

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -3,8 +3,10 @@
     <city-header></city-header>
     <city-search></city-search>
     <city-list :cities="cities"
-               :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+               :hot="hotCities"
+               :letter="letter"></city-list>
+    <city-alphabet :cities="cities"
+                   @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 <script>
@@ -23,6 +25,7 @@ export default {
   },
   data() {
     return {
+      letter: '',
       cities: {},
       hotCities: []
     }
@@ -38,6 +41,9 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange(letter) {
+      this.letter = letter
     }
   },
   mounted() {
